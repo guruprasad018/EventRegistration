c62d46ed80aac6faf1ce4fbae1050ab795532609
GuruprasadRamanna <guruprasad.ramanna@gmail.com>
2017-01-29 16:30:42 -0800

    Added Event data as service

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,47 +1,9 @@
 'use strict';
 
 eventsApp.controller("EventController",
-    function EventController($scope) {
+    function EventController($scope, eventData) {
        
-        $scope.event = {
-            name: 'Angular Boot Camp',
-            date: '1/28/2017',
-            time: '3:00 pm',
-            location: {
-                address: 'Guru\' home',
-                city: 'San diego',
-                state: 'CA'
-            },
-            imageUrl: 'img/angularjs-logo.png',
-            sessions: [{
-                    name: 'Directive MasterClass',
-                    creatorName: 'Bob Smith',
-                    duration: 1,
-                    level: 'Advanced',
-                    abstract: 'In this session you will learn the ins and out of directives!',
-                    upVoteCount: 0
-
-                },
-                {
-                    name: 'Scopes for fun and profit',
-                    creatorName: 'John Doe',
-                    duration: 3,
-                    level: 'Introductory',
-                    abstract: 'This session will take a closer look at scope',
-                    upVoteCount: 0
-
-                },
-                {
-                    name: 'Well Behaved controllers',
-                    creatorName: 'Jane Doe',
-                    duration: 4,
-                    level: 'Intermediate',
-                    abstract: 'controller are the beginning of everything',
-                    upVoteCount: 0
-
-                }
-            ]
-        }
+        $scope.event = eventData.event;
 
         $scope.upVoteSession = function(session) {
             session.upVoteCount++;
